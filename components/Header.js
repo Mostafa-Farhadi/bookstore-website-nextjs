0ce9d9be444dc22d13f6ac96c1c36818c2793e62
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-05 18:35:22 +0430

    Create search input and cart element link for cart page and style them

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,8 @@ const Header = () => {
     return ( 
         <header id="header">
             <Link href="/"><a>Home</a></Link>
+            <input type="search" name="search" id="search" placeholder="Search your favorite book"/>
+            <Link href="/cart"><a>Cart</a></Link>
         </header>
     );
 }
