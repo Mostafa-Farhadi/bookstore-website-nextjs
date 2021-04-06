1e31d6a01ea1ede030494d03d35e52105f80f268
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-06 13:07:02 +0430

    Add some book cards to home page and style them

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Book from '../components/Book'
+
 const Home = () => {
   return ( 
       <section id="home">
           <img src="image/cover/cover.jpg" alt="cover"/>
+          <div className="books">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+          </div>
       </section>
   );
 }
