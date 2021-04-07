de7fbe2a2b822294c287c8be9de7a29abc848f93
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-07 12:09:50 +0430

    Add price for every book to api

--- pages/api/books.js
+++ pages/api/books.js
@@ -5,6 +5,7 @@ export default (req, res) => {
       id: 1,
       Author: "Francis Scott Key Fitzgerald",
       nationality: "American",
+      price: "$10.99",
       Genres: ["Novel", "Fiction", "Tragedy"]
     },
     {
@@ -12,6 +13,7 @@ export default (req, res) => {
       id: 2,
       Author: "William Shakespeare",
       nationality: "British",
+      price: "$12.99",
       Genres: ["Tragedy", "Drama"],
     },
     {
@@ -19,6 +21,7 @@ export default (req, res) => {
       id: 3,
       Author: "Marcel Proust",
       nationality: "France",
+      price: "$15.99",
       Genres: ["Modern literature", "Philosophical fiction", "Social novel", "Fictional autobiography"]
     },
     {
@@ -26,6 +29,7 @@ export default (req, res) => {
       id: 4,
       Author: "Thomas Mann",
       nationality: "German",
+      price: "$13.99",
       Genres: ["Novel", "Fiction", "Bildungsroman"]
     },
     {
@@ -33,6 +37,7 @@ export default (req, res) => {
       id: 5,
       Author: "Laurence Sterne",
       nationality: "Irish",
+      price: "$11.99",
       Genres: ["Novel", "Satire", "Humour", "Biography", "Stream of consciousness", "Experimental literature", "Humorous fiction"]
     },
     {
@@ -40,6 +45,7 @@ export default (req, res) => {
       id: 6,
       Author: "Herman Melville",
       nationality: "American ",
+      price: "$15.99",
       Genres: ["Novel", "Adventure fiction", "Epic", "Psychological fiction", "Nautical fiction"]
     },
     {
@@ -47,6 +53,7 @@ export default (req, res) => {
       id: 7,
       Author: "Emily Brontë",
       nationality: "British",
+      price: "$14.99",
       Genres: ["Novel", "Gothic fiction", "Tragedy"]
     },
     {
@@ -54,6 +61,7 @@ export default (req, res) => {
       id: 8,
       Author: "Gustave Flaubert",
       nationality: "French",
+      price: "$10.99",
       Genres: ["Novel", "Literary realism", "Modern literature", "Literary fiction"]
     },
     {
@@ -61,6 +69,7 @@ export default (req, res) => {
       id: 9,
       Author: "Günter Grass",
       nationality: "German",
+      price: "$16.99",
       Genres: ["War", "Drama", "Fairy tale", "Datk comedy", "Political drama"]
     },
     {
@@ -68,6 +77,7 @@ export default (req, res) => {
       id: 10,
       Author: "James Joyce",
       nationality: "Irish",
+      price: "$18.99",
       Genres: ["Novel", "Modern literature", "Autobiography", "Bildungsroman", "Autobiographical novel", "Künstlerroman", "Biographical fiction", "Fictional autobiography", "Methaphysical fiction"]
     },
     {
@@ -75,6 +85,7 @@ export default (req, res) => {
       id: 11,
       Author: "J. D. Salinger",
       nationality: "American",
+      price: "$19.99",
       Genres: ["Novel", "Young adult fiction", "Bildungsroman", "Coming-of-age story", "Literary realism", "First-person narrative"]
     },
     {
@@ -82,6 +93,7 @@ export default (req, res) => {
       id: 12,
       Author: "Lewis Carroll",
       nationality: "British",
+      price: "$18.99",
       Genres: ["Novel", "Children's literature", "Fantasy Fiction", "Literary nonsense", "Magical Realism", "Absurdist fiction", "Fantastique"]
     },
     {
@@ -89,6 +101,7 @@ export default (req, res) => {
       id: 13,
       Author: "Voltaire",
       nationality: "French",
+      price: "$17.99",
       Genres: ["Novel", "Satire", "Philosophical fiction", "Tragedy", "Picaresque novel", "Bildungsroman", "Political fiction"]
     },
     {
@@ -96,6 +109,7 @@ export default (req, res) => {
       id: 14,
       Author: "Erich Maria Remarque",
       nationality: "German",
+      price: "$15.99",
       Genres: ["Novel", "Fiction", "War story", "Roman à clef"]
     },
     {
@@ -103,6 +117,7 @@ export default (req, res) => {
       id: 15,
       Author: "Bram Stoker",
       nationality: "Irish",
+      price: "$16.99",
       Genres: ["Novel", "Horror fiction", "Gothic fiction", "Graphic novel", "Fantasy fiction", "Epistolary novel", "Non-fiction", "Invasion literature", "Fantastique"]
     },
     {
@@ -110,6 +125,7 @@ export default (req, res) => {
       id: 16,
       Author: "Joseph Heller",
       nationality: "American",
+      price: "$20.99",
       Genres: ['Novel', "Satire", "Historical fiction", "War story", "Dark comedy", "Humorous Fiction", "Absurdist fiction"]
     },
     {
@@ -117,6 +133,7 @@ export default (req, res) => {
       id: 17,
       Author: "Virginia Woolf",
       nationality: "British",
+      price: "$19.99",
       Genres: ["Novel", "Fiction"]
     },
     {
@@ -124,6 +141,7 @@ export default (req, res) => {
       id: 18,
       Author: "Victor Hugo",
       nationality: "French",
+      price: "$13.99",
       Genres: ["Novel", "Historical Fiction", "Romance novel", "Tragedy", "Mystery", "Epic"]
     },
     {
@@ -131,6 +149,7 @@ export default (req, res) => {
       id: 19,
       Author: "Thomas Mann",
       nationality: "German",
+      price: "$17.99",
       Genres: ["Novel", "Fiction", "Family saga"]
     },
     {
@@ -138,6 +157,7 @@ export default (req, res) => {
       id: 20,
       Author: "Oscar Wilde",
       nationality: "Irish",
+      price: "$14.99",
       Genres: ["Gothic fiction", "Dark fantasy", "Philosophical fiction"]
     }
   ])
