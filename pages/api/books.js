4894900c6124c0c88fbde8f06959a32b8c02c3b1
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-08 11:48:49 +0430

    Use lowercase fot naming key parts of api

--- pages/api/books.js
+++ pages/api/books.js
@@ -2,163 +2,163 @@ export default (req, res) => {
   res.status(200).json([
     {
       name: "The Great Gatsby",
-      id: 1,
-      Author: "Francis Scott Key Fitzgerald",
+      id: 0,
+      author: "Francis Scott Key Fitzgerald",
       nationality: "American",
-      price: "$10.99",
-      Genres: ["Novel", "Fiction", "Tragedy"]
+      price: "10.99",
+      genres: ["Novel", "Fiction", "Tragedy"]
     },
     {
       name: "Hamlet",
-      id: 2,
-      Author: "William Shakespeare",
+      id: 1,
+      author: "William Shakespeare",
       nationality: "British",
-      price: "$12.99",
-      Genres: ["Tragedy", "Drama"],
+      price: "12.99",
+      genres: ["Tragedy", "Drama"],
     },
     {
       name: "In Search of Lost Time",
-      id: 3,
-      Author: "Marcel Proust",
+      id: 2,
+      author: "Marcel Proust",
       nationality: "France",
-      price: "$15.99",
-      Genres: ["Modern literature", "Philosophical fiction", "Social novel", "Fictional autobiography"]
+      price: "15.99",
+      genres: ["Modern literature", "Philosophical fiction", "Social novel", "Fictional autobiography"]
     },
     {
       name: "The Magic Mountain",
-      id: 4,
-      Author: "Thomas Mann",
+      id: 3,
+      author: "Thomas Mann",
       nationality: "German",
-      price: "$13.99",
-      Genres: ["Novel", "Fiction", "Bildungsroman"]
+      price: "13.99",
+      genres: ["Novel", "Fiction", "Bildungsroman"]
     },
     {
       name: "Tristram Shandy",
-      id: 5,
-      Author: "Laurence Sterne",
+      id: 4,
+      author: "Laurence Sterne",
       nationality: "Irish",
-      price: "$11.99",
-      Genres: ["Novel", "Satire", "Humour", "Biography", "Stream of consciousness", "Experimental literature", "Humorous fiction"]
+      price: "11.99",
+      genres: ["Novel", "Satire", "Humour", "Biography", "Stream of consciousness", "Experimental literature", "Humorous fiction"]
     },
     {
       name: "Moby Dick",
-      id: 6,
-      Author: "Herman Melville",
+      id: 5,
+      author: "Herman Melville",
       nationality: "American ",
-      price: "$15.99",
-      Genres: ["Novel", "Adventure fiction", "Epic", "Psychological fiction", "Nautical fiction"]
+      price: "15.99",
+      genres: ["Novel", "Adventure fiction", "Epic", "Psychological fiction", "Nautical fiction"]
     },
     {
       name: "Wuthering Heights",
-      id: 7,
-      Author: "Emily Brontë",
+      id: 6,
+      author: "Emily Brontë",
       nationality: "British",
-      price: "$14.99",
-      Genres: ["Novel", "Gothic fiction", "Tragedy"]
+      price: "14.99",
+      genres: ["Novel", "Gothic fiction", "Tragedy"]
     },
     {
       name: "Madame Bovary",
-      id: 8,
-      Author: "Gustave Flaubert",
+      id: 7,
+      author: "Gustave Flaubert",
       nationality: "French",
-      price: "$10.99",
-      Genres: ["Novel", "Literary realism", "Modern literature", "Literary fiction"]
+      price: "10.99",
+      genres: ["Novel", "Literary realism", "Modern literature", "Literary fiction"]
     },
     {
       name: "The Tin Drum",
-      id: 9,
-      Author: "Günter Grass",
+      id: 8,
+      author: "Günter Grass",
       nationality: "German",
-      price: "$16.99",
-      Genres: ["War", "Drama", "Fairy tale", "Datk comedy", "Political drama"]
+      price: "16.99",
+      genres: ["War", "Drama", "Fairy tale", "Datk comedy", "Political drama"]
     },
     {
       name: "A Portrait of the Artist as a Young Man",
-      id: 10,
-      Author: "James Joyce",
+      id: 9,
+      author: "James Joyce",
       nationality: "Irish",
-      price: "$18.99",
-      Genres: ["Novel", "Modern literature", "Autobiography", "Bildungsroman", "Autobiographical novel", "Künstlerroman", "Biographical fiction", "Fictional autobiography", "Methaphysical fiction"]
+      price: "18.99",
+      genres: ["Novel", "Modern literature", "Autobiography", "Bildungsroman", "Autobiographical novel", "Künstlerroman", "Biographical fiction", "Fictional autobiography", "Methaphysical fiction"]
     },
     {
       name: "The Catcher in the Rye",
-      id: 11,
-      Author: "J. D. Salinger",
+      id: 10,
+      author: "J. D. Salinger",
       nationality: "American",
-      price: "$19.99",
-      Genres: ["Novel", "Young adult fiction", "Bildungsroman", "Coming-of-age story", "Literary realism", "First-person narrative"]
+      price: "19.99",
+      genres: ["Novel", "Young adult fiction", "Bildungsroman", "Coming-of-age story", "Literary realism", "First-person narrative"]
     },
     {
       name: "Alice's Adventures in Wonderland",
-      id: 12,
-      Author: "Lewis Carroll",
+      id: 11,
+      author: "Lewis Carroll",
       nationality: "British",
-      price: "$18.99",
-      Genres: ["Novel", "Children's literature", "Fantasy Fiction", "Literary nonsense", "Magical Realism", "Absurdist fiction", "Fantastique"]
+      price: "18.99",
+      genres: ["Novel", "Children's literature", "Fantasy Fiction", "Literary nonsense", "Magical Realism", "Absurdist fiction", "Fantastique"]
     },
     {
       name: "Candide",
-      id: 13,
-      Author: "Voltaire",
+      id: 12,
+      author: "Voltaire",
       nationality: "French",
-      price: "$17.99",
-      Genres: ["Novel", "Satire", "Philosophical fiction", "Tragedy", "Picaresque novel", "Bildungsroman", "Political fiction"]
+      price: "17.99",
+      genres: ["Novel", "Satire", "Philosophical fiction", "Tragedy", "Picaresque novel", "Bildungsroman", "Political fiction"]
     },
     {
       name: "All Quiet on the Western Front",
-      id: 14,
-      Author: "Erich Maria Remarque",
+      id: 13,
+      author: "Erich Maria Remarque",
       nationality: "German",
-      price: "$15.99",
-      Genres: ["Novel", "Fiction", "War story", "Roman à clef"]
+      price: "15.99",
+      genres: ["Novel", "Fiction", "War story", "Roman à clef"]
     },
     {
       name: "Dracula",
-      id: 15,
-      Author: "Bram Stoker",
+      id: 14,
+      author: "Bram Stoker",
       nationality: "Irish",
-      price: "$16.99",
-      Genres: ["Novel", "Horror fiction", "Gothic fiction", "Graphic novel", "Fantasy fiction", "Epistolary novel", "Non-fiction", "Invasion literature", "Fantastique"]
+      price: "16.99",
+      genres: ["Novel", "Horror fiction", "Gothic fiction", "Graphic novel", "Fantasy fiction", "Epistolary novel", "Non-fiction", "Invasion literature", "Fantastique"]
     },
     {
       name: "Catch-22",
-      id: 16,
-      Author: "Joseph Heller",
+      id: 15,
+      author: "Joseph Heller",
       nationality: "American",
-      price: "$20.99",
-      Genres: ['Novel', "Satire", "Historical fiction", "War story", "Dark comedy", "Humorous Fiction", "Absurdist fiction"]
+      price: "20.99",
+      genres: ['Novel', "Satire", "Historical fiction", "War story", "Dark comedy", "Humorous Fiction", "Absurdist fiction"]
     },
     {
       name: "To the Lighthouse",
-      id: 17,
-      Author: "Virginia Woolf",
+      id: 16,
+      author: "Virginia Woolf",
       nationality: "British",
-      price: "$19.99",
-      Genres: ["Novel", "Fiction"]
+      price: "19.99",
+      genres: ["Novel", "Fiction"]
     },
     {
       name: "Les Misérables",
-      id: 18,
-      Author: "Victor Hugo",
+      id: 17,
+      author: "Victor Hugo",
       nationality: "French",
-      price: "$13.99",
-      Genres: ["Novel", "Historical Fiction", "Romance novel", "Tragedy", "Mystery", "Epic"]
+      price: "13.99",
+      genres: ["Novel", "Historical Fiction", "Romance novel", "Tragedy", "Mystery", "Epic"]
     },
     {
       name: "Buddenbrooks",
-      id: 19,
-      Author: "Thomas Mann",
+      id: 18,
+      author: "Thomas Mann",
       nationality: "German",
-      price: "$17.99",
-      Genres: ["Novel", "Fiction", "Family saga"]
+      price: "17.99",
+      genres: ["Novel", "Fiction", "Family saga"]
     },
     {
       name: "The Picture of Dorian Gray",
-      id: 20,
-      Author: "Oscar Wilde",
+      id: 19,
+      author: "Oscar Wilde",
       nationality: "Irish",
-      price: "$14.99",
-      Genres: ["Gothic fiction", "Dark fantasy", "Philosophical fiction"]
+      price: "14.99",
+      genres: ["Gothic fiction", "Dark fantasy", "Philosophical fiction"]
     }
   ])
 }
