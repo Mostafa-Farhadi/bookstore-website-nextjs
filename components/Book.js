226c8e1f274e506db2e8f8d9888adf0077e425ba
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-09 01:39:19 +0430

    Use link to access details page of each book

--- components/Book.js
+++ components/Book.js
@@ -1,14 +1,18 @@
+import Link from "next/link";
+
 const Book = (props) => {
-    const { url, name, author, price } = props
+    const { url, name, author, price, link } = props
     const priceBefore = parseInt(price) + 5
 
     return ( 
-        <div className="book">
-            <img src={url} alt="book"/>
-            <p>{name}</p>
-            <p>By {author}</p>
-            <p><span>${priceBefore}</span> &nbsp; ${price}</p>
-        </div>
+        <Link href={`/books/${link}`}>
+            <div className="book">
+                <img src={url} alt="book"/>
+                <p>{name}</p>
+                <p>By {author}</p>
+                <p><span>${priceBefore}</span> &nbsp; ${price}</p>
+            </div>
+        </Link>
     );
 }
 
