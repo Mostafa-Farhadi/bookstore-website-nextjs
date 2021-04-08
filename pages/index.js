cb7d7c5a13685d28552a641d988a471efee6981c
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-09 01:40:23 +0430

    Use id to pass link props to each book card

--- pages/index.js
+++ pages/index.js
@@ -12,24 +12,23 @@ const Home = ({ books }) => {
     return randomArr;
   }
   const res = getRanArr(books.length);
-  console.log(res);
   
   return ( 
       <section id="home">
           <img src="image/cover/cover.jpg" alt="cover"/>
           <h1>Best Seller</h1>
           <div className="books">
-            <Book url={`image/books/${books[res[0]].name}.jpg`} name={books[res[0]].name} author={books[res[0]].author} price={books[res[0]].price} />
-            <Book url={`image/books/${books[res[1]].name}.jpg`} name={books[res[1]].name} author={books[res[1]].author} price={books[res[1]].price} />
-            <Book url={`image/books/${books[res[2]].name}.jpg`} name={books[res[2]].name} author={books[res[2]].author} price={books[res[2]].price} />
-            <Book url={`image/books/${books[res[3]].name}.jpg`} name={books[res[3]].name} author={books[res[3]].author} price={books[res[3]].price} />
+            <Book url={`image/books/${books[res[0]].name}.jpg`} name={books[res[0]].name} author={books[res[0]].author} price={books[res[0]].price} link={books[res[0]].id} />
+            <Book url={`image/books/${books[res[1]].name}.jpg`} name={books[res[1]].name} author={books[res[1]].author} price={books[res[1]].price} link={books[res[1]].id} />
+            <Book url={`image/books/${books[res[2]].name}.jpg`} name={books[res[2]].name} author={books[res[2]].author} price={books[res[2]].price} link={books[res[2]].id} />
+            <Book url={`image/books/${books[res[3]].name}.jpg`} name={books[res[3]].name} author={books[res[3]].author} price={books[res[3]].price} link={books[res[3]].id} />
           </div>
       </section>
   );
 }
 
 export async function getStaticProps() {
-  const res = await fetch('http://localhost:3000/api/books')
+  const res = await fetch("http://localhost:3000/api/books")
   const books = await res.json()
   
   return {
