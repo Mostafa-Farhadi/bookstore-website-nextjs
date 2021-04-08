0e8e84df1d107a71ed17dbd16499769a0d7efba1
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-08 11:46:43 +0430

    1. Fetch books data from api to display them
    2. Use random numbers to display books randomly

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,42 @@
-import Book from '../components/Book'
+import Link from 'next/link';
+import Book from '../components/Book';
 
-const Home = () => {
+const Home = ({ books }) => {
+
+  function getRanArr(arrLength) {
+    let randomArr = [];
+    do {
+        let randomNum = Math.floor(Math.random() * arrLength);
+        randomArr = randomArr.indexOf(randomNum) > -1 ? randomArr : randomArr.concat(randomNum);
+    }while (randomArr.length < 4)
+    return randomArr;
+  }
+  const res = getRanArr(books.length);
+  console.log(res);
+  
   return ( 
       <section id="home">
           <img src="image/cover/cover.jpg" alt="cover"/>
+          <h1>Best Seller</h1>
           <div className="books">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book url={`image/books/${books[res[0]].name}.jpg`} name={books[res[0]].name} author={books[res[0]].author} price={books[res[0]].price} />
+            <Book url={`image/books/${books[res[1]].name}.jpg`} name={books[res[1]].name} author={books[res[1]].author} price={books[res[1]].price} />
+            <Book url={`image/books/${books[res[2]].name}.jpg`} name={books[res[2]].name} author={books[res[2]].author} price={books[res[2]].price} />
+            <Book url={`image/books/${books[res[3]].name}.jpg`} name={books[res[3]].name} author={books[res[3]].author} price={books[res[3]].price} />
           </div>
       </section>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/books')
+  const books = await res.json()
+  
+  return {
+    props: {
+      books,
+    },
+  }
 }
 
 export default Home;
