cc513a45b96c03feb6fed1d051d9a8544fd6548a
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-08 11:41:57 +0430

    Write missed slash in href of nationality page link

--- components/Sidenav.js
+++ components/Sidenav.js
@@ -6,7 +6,7 @@ const Sidenav = () => {
             <Link href="/"><a>Home</a></Link>
             <Link href="/genre"><a>Genre</a></Link>
             <Link href="/writer"><a>Writer</a></Link>
-            <Link href="nationality"><a>Nationality</a></Link>
+            <Link href="/nationality"><a>Nationality</a></Link>
         </nav>
     );
 }
