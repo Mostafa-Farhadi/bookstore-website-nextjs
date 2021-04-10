e0b908f02ba880ea6c706f535af5c425d02ddd69
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-10 15:02:34 +0430

    Add about section to each book

--- dataFile.js
+++ dataFile.js
@@ -5,7 +5,8 @@ export const data = [
         author: "Francis Scott Key Fitzgerald",
         nationality: "American",
         price: "10.99",
-        genres: ["Novel", "Fiction", "Tragedy"]
+        genres: ["Novel", "Fiction", "Tragedy"],
+        about: "The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, the novel depicts narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan."
     },
     {
         name: "Hamlet",
@@ -14,6 +15,7 @@ export const data = [
         nationality: "British",
         price: "12.99",
         genres: ["Tragedy", "Drama"],
+        about: "The Tragedy of Hamlet, Prince of Denmark, often shortened to Hamlet, is a tragedy written by William Shakespeare sometime between 1599 and 1601. It is Shakespeare's longest play, with 29,551 words."
     },
     {
         name: "In Search of Lost Time",
@@ -21,7 +23,8 @@ export const data = [
         author: "Marcel Proust",
         nationality: "France",
         price: "15.99",
-        genres: ["Modern literature", "Philosophical fiction", "Social novel", "Fictional autobiography"]
+        genres: ["Modern literature", "Philosophical fiction", "Social novel", "Fictional autobiography"],
+        about: "In Search of Lost Time, also translated as Remembrance of Things Past, is a novel in seven volumes by Marcel Proust. It is his most prominent work, known both for its length and its theme of involuntary memory; the most famous example of this is the 'episode of the madeleine,' which occurs early in the first volume."
     },
     {
         name: "The Magic Mountain",
@@ -29,7 +32,8 @@ export const data = [
         author: "Thomas Mann",
         nationality: "German",
         price: "13.99",
-        genres: ["Novel", "Fiction", "Bildungsroman"]
+        genres: ["Novel", "Fiction", "Bildungsroman"],
+        about: "The Magic Mountain is a novel by Thomas Mann, first published in German in November 1924. It is widely considered to be one of the most influential works of twentieth-century German literature. Mann started writing what was to become The Magic Mountain in 1912."
     },
     {
         name: "Tristram Shandy",
@@ -37,7 +41,8 @@ export const data = [
         author: "Laurence Sterne",
         nationality: "Irish",
         price: "11.99",
-        genres: ["Novel", "Satire", "Humour", "Biography", "Stream of consciousness", "Experimental literature", "Humorous fiction"]
+        genres: ["Novel", "Satire", "Humour", "Biography", "Stream of consciousness", "Experimental literature", "Humorous fiction"],
+        about: "The Life and Opinions of Tristram Shandy, Gentleman, also known as just Tristram Shandy, is a novel by Laurence Sterne. It was published in nine volumes, the first two appearing in 1759, and seven others following over the next seven years. It purports to be a biography of the eponymous character."
     },
     {
         name: "Moby Dick",
@@ -45,7 +50,8 @@ export const data = [
         author: "Herman Melville",
         nationality: "American ",
         price: "15.99",
-        genres: ["Novel", "Adventure fiction", "Epic", "Psychological fiction", "Nautical fiction"]
+        genres: ["Novel", "Adventure fiction", "Epic", "Psychological fiction", "Nautical fiction"],
+        about: "Moby-Dick; or, The Whale is an 1851 novel by American writer Herman Melville. The book is the sailor Ishmael's narrative of the obsessive quest of Ahab, captain of the whaling ship Pequod, for revenge on Moby Dick, the giant white sperm whale that on the ship's previous voyage bit off Ahab's leg at the knee."
     },
     {
         name: "Wuthering Heights",
@@ -53,7 +59,8 @@ export const data = [
         author: "Emily Brontë",
         nationality: "British",
         price: "14.99",
-        genres: ["Novel", "Gothic fiction", "Tragedy"]
+        genres: ["Novel", "Gothic fiction", "Tragedy"],
+        about: "Wuthering Heights is an 1847 novel by Emily Brontë, published under the pseudonym Ellis Bell. It concerns two families of the landed gentry living on the West Yorkshire moors, the Earnshaws and the Lintons, and their turbulent relationships with Earnshaw's adopted son, Heathcliff."
     },
     {
         name: "Madame Bovary",
@@ -61,7 +68,8 @@ export const data = [
         author: "Gustave Flaubert",
         nationality: "French",
         price: "10.99",
-        genres: ["Novel", "Literary realism", "Modern literature", "Literary fiction"]
+        genres: ["Novel", "Literary realism", "Modern literature", "Literary fiction"],
+        about: "Madame Bovary, originally published as Madame Bovary: Provincial Manners, is the debut novel of French writer Gustave Flaubert, published in 1856. The eponymous character lives beyond her means in order to escape the banalities and emptiness of provincial life."
     },
     {
         name: "The Tin Drum",
@@ -69,7 +77,8 @@ export const data = [
         author: "Günter Grass",
         nationality: "German",
         price: "16.99",
-        genres: ["War", "Drama", "Fairy tale", "Datk comedy", "Political drama"]
+        genres: ["War", "Drama", "Fairy tale", "Datk comedy", "Political drama"],
+        about: "The Tin Drum is a 1959 novel by Günter Grass. The novel is the first book of Grass's Danziger Trilogie. It was adapted into a 1979 film, which won both the 1979 Palme d'Or and the Academy Award for Best Foreign Language Film in 1980."
     },
     {
         name: "A Portrait of the Artist as a Young Man",
@@ -77,7 +86,8 @@ export const data = [
         author: "James Joyce",
         nationality: "Irish",
         price: "18.99",
-        genres: ["Novel", "Modern literature", "Autobiography", "Bildungsroman", "Autobiographical novel", "Künstlerroman", "Biographical fiction", "Fictional autobiography", "Methaphysical fiction"]
+        genres: ["Novel", "Modern literature", "Autobiography", "Bildungsroman", "Autobiographical novel", "Künstlerroman", "Biographical fiction", "Fictional autobiography", "Methaphysical fiction"],
+        about: "A Portrait of the Artist as a Young Man is the first novel of Irish writer James Joyce. A Künstlerroman written in a modernist style, it traces the religious and intellectual awakening of young Stephen Dedalus, Joyce's fictional alter ego, whose surname alludes to Daedalus, Greek mythology's consummate craftsman."
     },
     {
         name: "The Catcher in the Rye",
@@ -85,7 +95,8 @@ export const data = [
         author: "J. D. Salinger",
         nationality: "American",
         price: "19.99",
-        genres: ["Novel", "Young adult fiction", "Bildungsroman", "Coming-of-age story", "Literary realism", "First-person narrative"]
+        genres: ["Novel", "Young adult fiction", "Bildungsroman", "Coming-of-age story", "Literary realism", "First-person narrative"],
+        about: "The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951. It was originally intended for adults but is often read by adolescents for its themes of angst, alienation, and as a critique on superficiality in society. It has been translated widely."
     },
     {
         name: "Alice's Adventures in Wonderland",
@@ -93,7 +104,8 @@ export const data = [
         author: "Lewis Carroll",
         nationality: "British",
         price: "18.99",
-        genres: ["Novel", "Children's literature", "Fantasy Fiction", "Literary nonsense", "Magical Realism", "Absurdist fiction", "Fantastique"]
+        genres: ["Novel", "Children's literature", "Fantasy Fiction", "Literary nonsense", "Magical Realism", "Absurdist fiction", "Fantastique"],
+        about: "Alice's Adventures in Wonderland is an 1865 novel by English author Lewis Carroll. It tells of a young girl named Alice, who falls through a rabbit hole into a subterranean fantasy world populated by peculiar, anthropomorphic creatures. It is considered to be one of the best examples of the literary nonsense genre."
     },
     {
         name: "Candide",
@@ -101,7 +113,8 @@ export const data = [
         author: "Voltaire",
         nationality: "French",
         price: "17.99",
-        genres: ["Novel", "Satire", "Philosophical fiction", "Tragedy", "Picaresque novel", "Bildungsroman", "Political fiction"]
+        genres: ["Novel", "Satire", "Philosophical fiction", "Tragedy", "Picaresque novel", "Bildungsroman", "Political fiction"],
+        about: "Candide is a French satire first published in 1759 by Voltaire, a philosopher of the Age of Enlightenment. The novella has been widely translated, with English versions titled Candide: or, All for the Best (1759); Candide: or, The Optimist (1762); and Candide: or, Optimism (1947)."
     },
     {
         name: "All Quiet on the Western Front",
@@ -109,7 +122,8 @@ export const data = [
         author: "Erich Maria Remarque",
         nationality: "German",
         price: "15.99",
-        genres: ["Novel", "Fiction", "War story", "Roman à clef"]
+        genres: ["Novel", "Fiction", "War story", "Roman à clef"],
+        about: "All Quiet on the Western Front is a novel by Erich Maria Remarque, a German veteran of World War I. The book describes the German soldiers' extreme physical and mental stress during the war, and the detachment from civilian life felt by many of these soldiers upon returning home from the front."
     },
     {
         name: "Dracula",
@@ -117,7 +131,8 @@ export const data = [
         author: "Bram Stoker",
         nationality: "Irish",
         price: "16.99",
-        genres: ["Novel", "Horror fiction", "Gothic fiction", "Graphic novel", "Fantasy fiction", "Epistolary novel", "Non-fiction", "Invasion literature", "Fantastique"]
+        genres: ["Novel", "Horror fiction", "Gothic fiction", "Graphic novel", "Fantasy fiction", "Epistolary novel", "Non-fiction", "Invasion literature", "Fantastique"],
+        about: "Dracula is an 1897 Gothic horror novel by Irish author Bram Stoker. It introduced the character of Count Dracula and established many conventions of subsequent vampire fantasy."
     },
     {
         name: "Catch-22",
@@ -125,7 +140,8 @@ export const data = [
         author: "Joseph Heller",
         nationality: "American",
         price: "20.99",
-        genres: ['Novel', "Satire", "Historical fiction", "War story", "Dark comedy", "Humorous Fiction", "Absurdist fiction"]
+        genres: ['Novel', "Satire", "Historical fiction", "War story", "Dark comedy", "Humorous Fiction", "Absurdist fiction"],
+        about: "Catch-22 is a satirical war novel by American author Joseph Heller. He began writing it in 1953; the novel was first published in 1961."
     },
     {
         name: "To the Lighthouse",
@@ -133,7 +149,8 @@ export const data = [
         author: "Virginia Woolf",
         nationality: "British",
         price: "19.99",
-        genres: ["Novel", "Fiction"]
+        genres: ["Novel", "Fiction"],
+        about: "To the Lighthouse is a 1927 novel by Virginia Woolf. The novel centres on the Ramsay family and their visits to the Isle of Skye in Scotland between 1910 and 1920."
     },
     {
         name: "Les Misérables",
@@ -141,7 +158,8 @@ export const data = [
         author: "Victor Hugo",
         nationality: "French",
         price: "13.99",
-        genres: ["Novel", "Historical Fiction", "Romance novel", "Tragedy", "Mystery", "Epic"]
+        genres: ["Novel", "Historical Fiction", "Romance novel", "Tragedy", "Mystery", "Epic"],
+        about: "Les Misérables is a French historical novel by Victor Hugo, first published in 1862, that is considered one of the greatest novels of the 19th century. In the English-speaking world, the novel is usually referred to by its original French title."
     },
     {
         name: "Buddenbrooks",
@@ -149,7 +167,8 @@ export const data = [
         author: "Thomas Mann",
         nationality: "German",
         price: "17.99",
-        genres: ["Novel", "Fiction", "Family saga"]
+        genres: ["Novel", "Fiction", "Family saga"],
+        about: "Buddenbrooks is a 1901 novel by Thomas Mann, chronicling the decline of a wealthy north German merchant family over the course of four generations, incidentally portraying the manner of life and mores of the Hanseatic bourgeoisie in the years from 1835 to 1877."
     },
     {
         name: "The Picture of Dorian Gray",
@@ -157,6 +176,7 @@ export const data = [
         author: "Oscar Wilde",
         nationality: "Irish",
         price: "14.99",
-        genres: ["Gothic fiction", "Dark fantasy", "Philosophical fiction"]
+        genres: ["Gothic fiction", "Dark fantasy", "Philosophical fiction"],
+        about: "The Picture of Dorian Gray is a Gothic and philosophical novel by Oscar Wilde, first published complete in the July 1890 issue of Lippincott's Monthly Magazine. Fearing the story was indecent, prior to publication the magazine's editor deleted roughly five hundred words without Wilde's knowledge."
     }
 ]
