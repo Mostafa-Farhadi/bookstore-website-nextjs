a26627028400be67d5bf13c30f1f43fab1e2c4d7
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-11 15:31:01 +0430

    Create a page to classified books according to their author nationalities and then style it

--- pages/nationality.js
+++ pages/nationality.js
@@ -1,9 +1,61 @@
-const Nationality = () => {
+import { useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
+import Book from '../components/Book';
+
+const Nationality = ({ books }) => {
+    const [bookDetails, setBookDetails] = useState(null)
+
+    let nationalities = [];
+    for (let i = 0; i < (books.length - 1); i++) {
+        const nationality = books[i].nationality;
+            nationalities = nationalities.indexOf(nationality) > -1 ? nationalities : nationalities.concat(nationality);
+    }
+
+    const nationalityHandler = (event) => {
+        const nationality = event.target.value;
+        let booksArr = []
+        for (let k = 0; k < books.length; k++) {
+            const nationalityArr = books[k].nationality;
+            nationalityArr.indexOf(nationality) > -1 ? booksArr.push(books[k]) : '';
+        }
+        setBookDetails(booksArr)
+    }
+    
     return ( 
         <section id="nationality">
-            <p>Nationality</p>
+            <select name="nationality" id="nationality" size="5">
+            {nationalities.sort().map(nationality => (
+                <option value={nationality} key={uuidv4()} onClick={nationalityHandler}>
+                    {nationality}
+                </option>
+                ))}
+            </select>
+            <div className="books-container">
+                {
+                    bookDetails === null ? '' : bookDetails.map(book => (
+                        <Book key={book.id} 
+                            url={`image/books/${book.name}.jpg`} 
+                            name={book.name} 
+                            author={book.author} 
+                            price={book.price} 
+                            link={book.id}
+                        />
+                    ))
+                }
+            </div>
         </section>
     );
+}
+
+export async function getStaticProps() {
+    const res = await fetch("http://localhost:3000/api/books")
+    const books = await res.json()
+    
+    return {
+        props: {
+            books,
+        },
+    }
 }
 
 export default Nationality;
