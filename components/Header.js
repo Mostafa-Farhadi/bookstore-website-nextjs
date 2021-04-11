f41411547c9a8048685390185c9c9aac9d78ce41
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-11 16:32:43 +0430

    Delete cart page temporary

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,6 @@ const Header = () => {
         <header id="header">
             <Link href="/"><a>Home</a></Link>
             <input type="search" name="search" id="search" placeholder="Search your favorite book"/>
-            <Link href="/cart"><a>Cart</a></Link>
         </header>
     );
 }
