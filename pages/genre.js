7d08743d43b2da5a3ee851edb6856668676bddc2
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-13 13:27:39 +0430

    Edit for loop limit to read all books, edit some naming to better readability, and turn url props of Book component to src

--- pages/genre.js
+++ pages/genre.js
@@ -6,29 +6,29 @@ const Genre = ({ books }) => {
     const [bookDetails, setBookDetails] = useState(null);
     const [selectedGenre, setSelectedGenre] = useState(null);
 
-    let totalGenresArr = [];
-    for (let i = 0; i < (books.length - 1); i++) {
+    let totalGenres = [];
+    for (let i = 0; i < books.length; i++) {
         const genresArr = books[i].genres;
-        for (let j = 0; j < (genresArr.length - 1); j++) {
-            totalGenresArr = totalGenresArr.indexOf(genresArr[j]) > -1 ? totalGenresArr : totalGenresArr.concat(genresArr[j]);
+        for (let j = 0; j < genresArr.length; j++) {
+            totalGenres = totalGenres.indexOf(genresArr[j]) > -1 ? totalGenres : totalGenres.concat(genresArr[j]);
         }
     }
 
     const genreHandler = (event) => {
         const genre = event.target.value;
-        setSelectedGenre(genre)
-        let booksArr = []
-        for (let k = 0; k < (books.length - 1); k++) {
+        setSelectedGenre(genre);
+        let booksArr = [];
+        for (let k = 0; k < books.length; k++) {
             const genresArr = books[k].genres;
-            genresArr.indexOf(genre) > -1 ? booksArr.push(books[k]) : '';
+            genresArr.indexOf(genre) > -1 ? booksArr.push(books[k]) : booksArr;
         }
-        setBookDetails(booksArr)
+        setBookDetails(booksArr);
     }
 
     return (
         <section id="genre">
             <select name="genres" id="genres" size="8">
-            {totalGenresArr.sort().map(genre => (
+            {totalGenres.sort().map(genre => (
                 <option value={genre} key={uuidv4()} onClick={genreHandler}>{genre}</option>
                 ))}
             </select>
@@ -37,7 +37,7 @@ const Genre = ({ books }) => {
                 {
                     bookDetails === null ? '' : bookDetails.map(book => (
                         <Book key={book.id} 
-                            url={`image/books/${book.name}.jpg`} 
+                            src={`image/books/${book.name}.jpg`} 
                             name={book.name} 
                             author={book.author} 
                             price={book.price} 
