e93c208ff461e5f2fc0d6385fb7e15f08e2032dd
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-13 13:18:58 +0430

    Change destination of imported data and index of data array

--- pages/api/books/[id].js
+++ pages/api/books/[id].js
@@ -1,9 +1,9 @@
-import { data } from '../../../dataFile';
+import { data } from '../../../data';
 
 export default (req, res) => {
     const {
         query: {id}
     } = req;
 
-    res.status(200).json(data[id]);
+    res.status(200).json(data[id - 1]);
 };
