755555b9b6bf1603109522727f504e55154026d9
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-13 13:21:28 +0430

    Change name of url props and turn it to scr

--- components/Book.js
+++ components/Book.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
 const Book = (props) => {
-    const { url, name, author, price, link } = props
+    const { src, name, author, price, link } = props
     const priceBefore = parseInt(price) + 5
 
     return ( 
         <Link href={`/books/${link}`}>
             <div className="book">
-                <img src={url} alt="book"/>
+                <img src={src} alt="book"/>
                 <p>{name}</p>
                 <p>By {author}</p>
                 <p><span>${priceBefore}</span> &nbsp; ${price}</p>
