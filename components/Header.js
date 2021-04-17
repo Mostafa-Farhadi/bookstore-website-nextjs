d7c8ab5372d3efcfd567c1852ac1be4aa8ed3443
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-17 18:03:59 +0430

    Add icon to header linka and remove texts of link for screen size less than 640px

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,18 @@ import Link from 'next/link'
 const Header = () => {
     return ( 
         <header id="header">
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/genre"><a>Genre</a></Link>
-            <Link href="/author"><a>Author</a></Link>
-            <Link href="/nationality"><a>Nationality</a></Link>
+            <Link href="/">
+                <a><img src="/image/icon/home.png" alt="home"/><span>Home</span></a>
+            </Link>
+            <Link href="/genre">
+                <a><img src="/image/icon/genre.png" alt="genre"/><span>Genre</span></a>
+            </Link>
+            <Link href="/author">
+                <a><img src="/image/icon/author.png" alt="author"/><span>Author</span></a>
+            </Link>
+            <Link href="/nationality">
+                <a><img src="/image/icon/nationality.png" alt="nationality"/><span>Nationality</span></a>
+            </Link>
         </header>
     );
 }
