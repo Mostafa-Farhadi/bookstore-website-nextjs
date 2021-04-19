96c35e337036f5e08ef36bb8e79098a0e59346bc
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-19 18:48:23 +0430

    Change behavior of autocomplete search. Only those books are suggested that match their firsts letters with entered text. Also use a form tag to wrap input field and set form autocomplete attribute to false to prevent default input autocomplete behavoir. And style autocomplete suggestions by changing color and font weight ofmatched letters.

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 const SearchBox = ({ books }) => {
     const [suggests, setSuggests] = useState([]);
+    const [enteredText, setEnteredText] = useState("")
 
     let booksName = [];
     for (let i = 0; i < books.length; i++) {
@@ -11,31 +12,39 @@ const SearchBox = ({ books }) => {
     }
 
     const suggestHandler = event => {
-        const name = event.target.value.toLowerCase();
-        if (name !== "") {
+        const name = event.target.value;
+        setEnteredText(name);
+        const nameLowerCase = event.target.value.toLowerCase();
+        if (nameLowerCase !== "") {
             let suggestionsArr = [];
             for (let i = 0; i < booksName.length; i++) {
-            booksName[i].toLowerCase().indexOf(name) > -1 ? suggestionsArr.push([booksName[i], i]) : suggestionsArr;
+                if (booksName[i].toLowerCase().indexOf(nameLowerCase) > -1) {
+                    suggestionsArr.push([booksName[i].replace(booksName[i].substr(0, nameLowerCase.length), ""), i]);
+                } else {
+                    suggestionsArr;
+                }
             }
             setSuggests(suggestionsArr);
         } else {
-            setSuggests([])
+            setSuggests([]);
         }
     }
 
     return ( 
         <section className="search-box">
-            <input 
-                type="search" 
-                name="search" 
-                id="search" 
-                placeholder="Search your favourite book..."
-                onChange={event => suggestHandler(event)}
-            />
+            <form autoComplete="off">
+                <input 
+                    type="search" 
+                    name="search" 
+                    id="search" 
+                    placeholder="Search your favourite book..."
+                    onChange={event => suggestHandler(event)}
+                />
+            </form>
             <div>
                 {suggests.map(suggest => (
                     <Link href={`/books/${suggest[1] + 1}`} key={uuidv4()}>
-                        <a>{suggest[0]}</a>
+                        <a><span>{enteredText}</span>{suggest[0]}</a>
                     </Link>
                 ))}
             </div>
