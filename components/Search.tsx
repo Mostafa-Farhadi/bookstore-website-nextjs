e16050afd26e4dab63e2fa3dbceaba7229ce6a5a
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-05-13 11:58:05 +0430

    Use foreach loop instead od for loop

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import Style from '../styles/components/search.module.scss';
-import { IEvent, IProps } from "../interfaces";
+import { IEvent, IProps, IBooks } from "../interfaces";
 
 /*
 This is Search component. It is added to each page to enable user search books according to book name.
@@ -17,9 +17,9 @@ const Search = ({ books }: IProps) => {
     // An array to hold name of all books that are fetched from api and passed as props to Search book.
     const booksName: string[] = [];
     // Push all books name to 'booksName' array according to their index 
-    for (let i = 0; i < books.length; i++) {
-        booksName.push(books[i].name);
-    };
+    books.forEach((element: IBooks) => {
+        booksName.push(element.name);
+    });
 
     // A handler to set entered text as state to 'enteredText' and books and their ids according to entered text as state to 'suggests'.
     const suggestHandler = (event: IEvent): void => {
@@ -29,18 +29,18 @@ const Search = ({ books }: IProps) => {
         if (text !== "") {
             // An array to hold books and their ids according to entered text.
             const suggestionsArr: [string, number][] = [];
-            for (let i = 0; i < booksName.length; i++) {
+            booksName.forEach((element: string, index: number) => {
                 // Evaluate if books name are similar to entered text. 
-                if (booksName[i].slice(0, text.length).toLowerCase() === text) {
+                if (element.slice(0, text.length).toLowerCase() === text) {
                     // If books have similar words, their names and ids push to 'suggestionsArr' array.
-                    suggestionsArr.push([booksName[i].replace(booksName[i].substr(0, text.length), ""), i]);
+                    suggestionsArr.push([element.replace(element.substr(0, text.length), ""), index]);
                     // After removing similar words, the removed words are added to enteredText to show in the seuggestions. (They display bold).
-                    setEnteredText(booksName[i].slice(0, text.length));
+                    setEnteredText(element.slice(0, text.length));
                 } else {
                     // If there is no word similarity, the array doesn't change.
                     suggestionsArr;
                 }
-            }
+            });
             // All books with word similar to entered text add as state to 'suggests'.
             setSuggests(suggestionsArr);
         } else {
